7f6559e1080031dd30d084f730d52f3dda7c72b8
ovoroshylo <ovoroshylo3@gmail.com>
2022-10-26 23:55:31 +0300

    update layout

--- pages/admin.js
+++ pages/admin.js
@@ -13,7 +13,7 @@ import {useState} from "react"
 import { useEffect } from 'react'
 import {useRouter} from 'next/router'
 import axios from "axios"
-import Link from "next/link"
+import Link from "next/link" 
 const admin = ({dark,isMobileOpen,_allProjects,_allNews}) => {
     
     const [twitter, setTwitter] = useState(0);
